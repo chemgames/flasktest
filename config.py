630de5e0afb960b6a261bfaed756ef2b1d98a0e5
Olgmuth <poolmuth@gmail.com>
2019-05-31 14:28:22 +0200

    servertest7

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL') or 'postgresql://localhost/users'
+    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL') or 'postgresql:///users'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
